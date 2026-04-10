298a7b81ef992270cd89e22fe4cf3b6a86ac9048
TheRoy-99 <theroymrodriguez@gmail.com>
2026-04-10 11:10:07 -0500

    feat(users): add UsersModule with CRUD and SafeUser type

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,8 +3,9 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService],
+  exports: [UsersService], //Esto asegura que AuthService pueda usarlo
 })
 export class UsersModule {}
